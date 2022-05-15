63ea6f4d18f619764c9367436a2e2354b4bda261
Mesrop Mkhitaryan <mkhitaryanmesrop7@gmail.com>
2022-05-15 20:21:32 +0500

    first commit

--- src/homework/ArrayUtil.java
+++ src/homework/ArrayUtil.java
@@ -4,9 +4,11 @@ public class ArrayUtil<average> {
     public static void main(String[] args) {
         int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
         // print arrays allelements
+        System.out.print("all elements of array ");
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i]+" ");
         }
+        System.out.println();
         //biggest number
         int biggest = array[0];
         for (int i = 0; i < array.length; i++) {
@@ -14,7 +16,7 @@ public class ArrayUtil<average> {
                 biggest = array[i];
             }
         }
-        System.out.println(biggest);
+        System.out.println("biggest number = " + biggest);
         // smallest number
         int smallest = array[0];
         for (int i = 0; i < array.length; i++) {
@@ -22,39 +24,43 @@ public class ArrayUtil<average> {
                 smallest = array[i];
             }
         }
-        System.out.println(smallest);
+        System.out.println("smallest number = " + smallest);
         //Տպեք մասիվի բոլոր զույգ էլեմենտները,
+        System.out.print("print even numbers ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                System.out.println(array[i]);
+                System.out.print(array[i]+" ");
             }
 
         }
+        System.out.println();
 ////Տպեք մասիվի բոլոր կենտ էլեմենտները։
+        System.out.print("print odd numbers ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 != 0) {
-                System.out.println(array[i]);
+                System.out.print(array[i]+" ");
             }
 
         }
+        System.out.println();
         //Տպեք զույգերի քանակը։
-        int zuyg = 0;
+        int countOfEvenNums = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                zuyg++;
+                countOfEvenNums++;
             }
         }
-        System.out.println(zuyg);
+        System.out.println("quantity of even numbers = "+ countOfEvenNums);
         //Տպեք կենտերի քանակը
-        int kent = 0;
+        int countOfOddNums = 0;
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 != 0) {
-                kent++;
+                countOfOddNums++;
             }
 
         }
-        System.out.println(kent);
+        System.out.println("quantity of odd numbers = " + countOfOddNums);
         // Տպեք մասիվի էլեմենտների գումարը։ և մասիվի բոլոր թվերի միջին թվաբանականը (բոլոր էլեմենտների գումարը / էլեմենտների քանակի վրա)
         int sum = 0;
         int average = 0;
@@ -63,8 +69,8 @@ public class ArrayUtil<average> {
 
             average = sum / array.length;
         }
-        System.out.println(sum);
-        System.out.println(average);
+        System.out.println("sum of array = " + sum);
+        System.out.println("average number of array = " + average);
 
     }
 
